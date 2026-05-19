9db5e838af2fae14be326d31063271f2c6a8d544
Arian Gogani <agbusiness195@gmail.com>
2026-05-18 23:58:35 -0700

    rename server.js to fix Vercel static file routing

---
+++ packages/core/postinstall.js
@@ -0,0 +1,3 @@
+console.log('\n  \x1b[36m★\x1b[0m  Thanks for installing @nobulex/core');
+console.log('  \x1b[36m★\x1b[0m  Trust Capital for AI agents. Credit scores for machines.');
+console.log('  \x1b[36m★\x1b[0m  If this is useful, star us: \x1b[4mhttps://github.com/arian-gogani/nobulex\x1b[0m\n');
